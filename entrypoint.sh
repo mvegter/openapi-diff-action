499534d90cc47a459c5e812629f6e81a80859026
Martijn Vegter <martijn@martijnvegter.com>
2020-03-01 16:26:19 +0100

    feat: using npx openapi-diff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -l
 
-echo "TODO: Verify change of spec file on location '$1'"
+echo "cd $GITHUB_WORKSPACE" | sh
+echo "cp -r $GITHUB_WORKSPACE ../previous" | sh
+
+echo "cd ../previous" | sh
+echo "git reset --hard HEAD~" | sh
+
+echo "cd $GITHUB_WORKSPACE" | sh
+echo "npx openapi-diff $1 ../previous/$1" | sh
