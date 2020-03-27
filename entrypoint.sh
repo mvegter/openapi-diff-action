432d02657a088980a67c9e79b85b9d5c463d2e2d
Martijn Vegter <martijn@martijnvegter.com>
2020-03-27 16:34:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh -l
 
-echo "cd $GITHUB_WORKSPACE" | sh
-echo "cp -r $GITHUB_WORKSPACE ../previous" | sh
-
-echo "cd ../previous" | sh
-echo "git reset --hard HEAD~" | sh
-
-echo "cd $GITHUB_WORKSPACE" | sh
-echo "npx openapi-diff $1 ../previous/$1" | sh
+echo "npx openapi-diff $1 $2" | sh
